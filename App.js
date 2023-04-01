c6c9ea463517d517e24575ce6550eb8ceea1cbaf
BuildTools <unconfigured@null.spigotmc.org>
2023-04-01 12:06:00 -0400

    redone many things

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+import {styles} from "style.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    null
   );
 }
 
+
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +17,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textbox: {
+    margin: 5,
+    borderWidth: 5,
+    borderRadius: 10,
+  },
 });
+*/
